e5dc950572e4d6fc204d0bdac24222cc67b2c6dd
ibrahimchida <91400620+ibrahimchida@users.noreply.github.com>
2021-09-26 13:48:10 -0500

    Added comments and improved Docstring.

--- company_details.py
+++ company_details.py
@@ -1,33 +1,40 @@
 def read_company_names_produce_details() -> None:
     """Reads a file with company names, and then creates and writes to another
     file a few details of each company. The details it provides are the company's
-    row in the file, the company's name, and its acronym.
+    row in the file, the company's name, and its acronym. The acronym consists of the
+    first letter of each word in the title, excluding the ones in the cut_out list.
+    if the company name has 3 or more words, then the first letters are used. But if
+    it has less then 3, then the first 2 letters of the first word in the title are
+    used along with the first letter of the second word if there is one.
     """
     companies = open("Companies.csv").read()
     company_file = open("company_file.csv", "w")
     company_list = companies.split("\n")
     count = 0
-    cut_out = ["and", "if", "or", "at", "but", "for"]  # list of words to exclude from capitalization.
+    cut_out = ["and", "if", "or", "at", "but", "for", "in", "to"]  # list of words to exclude from capitalization.
 
     for company in company_list:
         if company == "":
             continue
-        count += 1
+        count += 1  # count increases for every company.
         acronym = ""
-        split_company = company.split()
+        split_company = company.split()      # splits company name into a list of words.
         for word in split_company:
-            if word not in cut_out:
-                if len(split_company) >= 3:
-                    acronym += word[0].upper()
+            if word not in cut_out:  # checks if word is in the cut_out list.
+                if len(split_company) >= 3:         # checks if company name has 3 or more words.
+                    acronym += word[0].upper()  # concatenates the first letter of the word to the acronym string.
                 else:
-                    if word == split_company[0]:
-                        acronym += word[:2].upper()
+                    if word == split_company[0]:  # if the name has less than 3 words, the program checks if
+                        # the wor being tested is the first one in the title.
+                        acronym += word[:2].upper()           # if so, then the program concatenates the first 2
+                        # letters of the first word to the acronym string.
                     else:
-                        acronym += word[0].upper()
+                        acronym += word[0].upper()  # if it's not, the program concatenates the fist letter of the
+                        # word to the acronym string.
 
-        total_list = [str(count), company, acronym, "\n"]
+        total_list = [str(count), company, acronym, "\n"]   # puts the stuff in one list
         new_total_list = ", ".join(total_list)
-        company_file.write(new_total_list)
+        company_file.write(new_total_list)  # writes to the new file.
 
     company_file.close()
 
