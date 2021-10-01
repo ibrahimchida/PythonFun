55078ae6045996e534a83a48c9d3d6bb5c6f4e37
ibrahimchida <91400620+ibrahimchida@users.noreply.github.com>
2021-09-30 20:11:06 -0500

    Fixed variable and finished else clause

--- flatten_file.py
+++ flatten_file.py
@@ -1,14 +1,16 @@
-def flatten(include_non_alpha=True, default_case=True):
+def flatten(exclude_non_alpha=True, default_case=True):
     file_to_read = open("contents.txt", "r")
     file_to_write_to = open("newcontents.txt", "w")
     file_contents = file_to_read.read().split()
     for word in file_contents:
         stripped_word = word.strip('1234567890')
-        if include_non_alpha:
+        if exclude_non_alpha:
             if default_case:
                 file_to_write_to.write(f"{stripped_word} \n")
             else:
                 file_to_write_to.write(f"{stripped_word} \n".lower())
-       
+        else:
+            file_to_write_to.write(f"{word} \n")
+
 
 flatten(True, False)
